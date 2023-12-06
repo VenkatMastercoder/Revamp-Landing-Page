c40949473b86ab609693b1e42f8810b2e0dd3f1b
Venkatesan <venkatesangunaraj@gmail.com>
2023-12-06 21:18:45 +0530

    🎉 Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "./assets/Animation.json";
+import { LogoBlack } from "./assets/img";
+
+function App() {
+  return (
+    <>
+      <section className="h-screen">
+        <div className="2xl:container mx-auto h-[100%] ">
+          <div className="w-[100%] mx-auto grid grid-cols-1 bg-black">
+            <div className="w-[90%] mx-auto flex justify-start items-center">
+              <img src={LogoBlack} alt="Logo" className="h-28" />
+            </div>
+          </div>
+          <div className="w-[90%] mx-auto grid grid-cols-1 md:grid-cols-2 h-[100%]">
+            <div className="flex flex-col justify-center items-center space-y-4 text-center">
+              <p className="text-xl md:text-3xl font-['Poppins']">Launching Soon!</p>
+              <h1 className="text-3xl md:text-5xl lg:text-7xl font-['Poppins']">TO REVAMP YOU</h1>
+              <button className="text-base md:text-xl font-['Poppins'] py-4 text-white 
+              bg-[#FF9F00] px-14 rounded-3xl">
+                Join our Journey
+              </button>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
+              <Lottie
+                animationData={groovyWalkAnimation}
+                loop={true}
+                className="h-96"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/img.js
@@ -0,0 +1,5 @@
+export const AnimationImg = "https://ik.imagekit.io/4z8covdo9/New%20Folder/Animation%20-%201701872581192.gif?updatedAt=1701873197607";
+
+export const Logo = "https://ik.imagekit.io/4z8covdo9/New%20Folder/REVAMP_LOGO.png?updatedAt=1701874321845";
+
+export const LogoBlack = "https://ik.imagekit.io/4z8covdo9/New%20Folder/REVAMP_LOGO_BLACK.png?updatedAt=1701875311264";
